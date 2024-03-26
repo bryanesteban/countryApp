4ec4ed34ff5bd7668dacdf4b5da4d1f48ef15778
bryanesteban <bryanesteban_123@hotmail.com>
2024-03-25 19:51:32 -0500

    slidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/components/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/components/pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomePageComponent
+  },
+  {
+    path:'about',
+    component: AboutPageComponent
+  },
+  {
+    path:'**',
+    redirectTo: 'home'
+  }
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
